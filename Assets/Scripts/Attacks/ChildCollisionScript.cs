4d3e72140ce384bc8f89bd15e959a14ac1b9fd9e
brice <brice.schumacher@ynov.com>
2018-11-10 11:27:46 +0100

    2 attacks and polygon collider

--- Assets/Scripts/Attacks/ChildCollisionScript.cs
+++ Assets/Scripts/Attacks/ChildCollisionScript.cs
@@ -6,8 +6,15 @@ public class ChildCollisionScript : MonoBehaviour {
     [SerializeField]
     Mow mowCollider;
     
-    void OnCollisionEnter2D(Collision2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
+        Debug.Log("Je suis dans le script ChildCollision et la collision a été detecté avec " + col.gameObject.name);
+
         mowCollider.OnMowCollision(col);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        Debug.Log("This is the end");
     }
 }
